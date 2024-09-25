d1eb0b69c02b7c8c1b6dd9d6309dd1c9b4abb95f
neethuchandhavarkar <woofwoofpoodle@gmail.com>
2024-09-25 20:02:19 +1000

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Jenkins Node API!');
+});
+
+app.listen(port, () => {
+  console.log(`App running at http://localhost:${port}`);
+});
+
+module.exports = app;
+
