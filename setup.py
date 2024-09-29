d5cc53c0939e63e9f8e9a2e838d0d20af97ab91f
neethuchandhavarkar <woofwoofpoodle@gmail.com>
2024-09-29 13:16:17 +1000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='my-python-app',
+    version='1.0',
+    packages=['my_python_app'],
+    install_requires=['flask'],
+    author='Your Name',
+    author_email='your_email@example.com'
+)
