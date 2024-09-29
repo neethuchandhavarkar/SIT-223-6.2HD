f4ae1933df52b17fcbad1866c0fea8c237df63bc
neethuchandhavarkar <woofwoofpoodle@gmail.com>
2024-09-29 14:28:41 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='1.0',
     packages=['my_python_app'],
     install_requires=['flask'],
-    author='Your Name',
-    author_email='your_email@example.com'
+    author='Neethu',
+    author_email='neethuchandhavarkar2003@gmail.com'
 )
