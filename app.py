cf920f8fdb5301373e15105408425b528af28af6
neethuchandhavarkar <woofwoofpoodle@gmail.com>
2024-10-05 13:52:33 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, jsonify
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    """Return a JSON response with a greeting message."""
     return jsonify({'message': 'Hello, World!'})
 
+
 if __name__ == '__main__':
-    """Run the application in debug mode."""
     app.run(debug=True)
